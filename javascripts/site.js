31efe3ac28d217456857865ab0dfae1f91e9a9e4
marioayala15 <marioayala15@users.noreply.github.com>
2026-06-26 11:25:52 +0000

    deploy: 59646af95b89176c41046c6a6cef0b48d858e698

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,8 @@
 // This is where it all goes :)
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.email-link').forEach(function(el) {
+    var addr = el.dataset.n + '@' + el.dataset.d;
+    el.textContent = addr;
+    el.href = 'mailto:' + addr;
+  });
+});
